21f2ca2cb0f22f58e810533d1c966943b9e03a3c
kaynanDev <kaynan2817@gmail.com>
2025-05-19 20:44:59 -0300

    criação de ambiente de desenvolvimento

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  Advice #<!-- Advice ID goes here -->
+
+  "<!-- Advice text goes here -->"
+
+  <div class="attribution">
+    Challenge by
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
+      >Frontend Mentor</a
+    >. Coded by <a href="#">Your Name Here</a>.
+  </div>
+</template>
+
+<style scoped></style>
